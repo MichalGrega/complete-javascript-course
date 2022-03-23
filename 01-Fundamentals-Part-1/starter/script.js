4a48eb94eb8e5d248daad243b7165e6f404c6e4c
Michal Grega <michal@gregasr.eu>
2022-03-23 16:24:15 +0100

    012 Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 // if (js === "amazing") alert("JavaScript is FUN!");
 
@@ -17,3 +18,31 @@ let $function = 27;
 let person = 'jonas';
 let PI = 3.1415
 
+//Comments
+*/
+
+// console.log(true);
+
+// Boolean value
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+// Reassigning the variable without let keyword
+javascriptIsFun = "YES";
+console.log(typeof javascriptIsFun);
+
+// Undefined - variable without a value
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+// type of null is object - javascript bug. Not corrected for legacy reasons
+console.log(typeof null);
