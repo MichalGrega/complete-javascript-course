624c017acd8cdbe85f76d9939c676f778c15cfed
Michal Grega <michal@gregasr.eu>
2022-03-23 16:57:44 +0100

    assignment - Data types

--- Assignments/ValuesAndVariables/script.js
+++ Assignments/ValuesAndVariables/script.js
@@ -1,3 +1,4 @@
+// Values and Variables
 let country = "Slovakia";
 let continent = "Europe";
 let population = 5.5;
@@ -5,3 +6,13 @@ let population = 5.5;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// Data types
+const isIsland = false;
+let language;
+console.log(
+  typeof isIsland,
+  typeof population,
+  typeof country,
+  typeof language
+);
