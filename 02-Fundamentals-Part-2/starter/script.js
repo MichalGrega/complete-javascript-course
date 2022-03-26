8622289b5f6f081f7f8159f05b68a7f64869c049
Michal Grega <michal@gregasr.eu>
2022-03-26 20:11:55 +0100

    041 Coding Challenge #2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -108,6 +108,7 @@ console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1950, 'Mike'));
 */
 
+/*
 const calcAverage = (array) => (array[0] + array[1] + array[2]) / 3;
 
 let scoreDolphins = calcAverage([44,23,71]);
@@ -131,3 +132,12 @@ scoreKoalas = calcAverage([23,34,27]);
 console.log(scoreDolphins, scoreKoalas);
 
 checkWinner(scoreDolphins, scoreKoalas);
+*/
+
+function calcTip(bill){
+  return bill>= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [125, 555, 44]
+const tips = bills.map(e => calcTip(e));
+const totals = bills.map(e => e + calcTip(e));
