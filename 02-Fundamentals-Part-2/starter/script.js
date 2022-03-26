f08a9884aea6ba59a044abb5547e7c8be77701aa
Michal Grega <michal@gregasr.eu>
2022-03-26 16:28:05 +0100

    033 Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,6 @@
 "use strict"; //Strict mode
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -8,3 +9,26 @@ if (hasDriversLicense) console.log("I can drive 🚗");
 
 // const interface = "Audio";
 // const private = 534;
+*/
+
+function logger () {
+  console.log("My name is Jonas");
+};
+
+logger();
+logger();
+logger();
+
+
+function fruitProcessor(apples, oranges) {
+  // console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+};
+
+const appleJuice = fruitProcessor(5,0);
+console.log(appleJuice);
+console.log(fruitProcessor(10,10));
+
+const appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice);
