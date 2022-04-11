c98f64a60777e138b4c433723811513b549c3268
Michal Grega <michal@gregasr.eu>
2022-04-11 19:59:03 +0200

    Pig game part I

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice rolled
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  // 2. Displaying the dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Checking for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // 4. Adding the score to the current player
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // 5. Switch to next player
+  }
+});
