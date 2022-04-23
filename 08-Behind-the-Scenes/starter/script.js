8061276cf3a1c89cdfd11649d22bc6b5b22cff58
Michal Grega <michal@gregasr.eu>
2022-04-23 19:29:20 +0200

    primitive types and reference types

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -32,3 +33,57 @@ function calcAge(birthYear) {
 const firstName = 'Jonas';
 calcAge(1991);
 // console.log(age);
+*/ ////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+
+const friend = me;
+friend.age = 27;
+
+console.log(me);
+console.log(friend);
+*/ ////////////
+
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+// reference types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before merriage:', jessica);
+console.log('After merriage:', marriedJessica);
+
+// copying objects
+const jessica2 = {
+  firstName: 'Jessica2',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+console.log('Before merriage:', jessica2);
+console.log('After merriage:', jessicaCopy);
