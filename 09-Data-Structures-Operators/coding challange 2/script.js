89e938c6552ae34332ed198739aed4934eb877ad
Michal Grega <michal@gregasr.eu>
2022-08-13 19:56:22 +0200

    refactured

--- 09-Data-Structures-Operators/coding challange 2/script.js
+++ 09-Data-Structures-Operators/coding challange 2/script.js
@@ -1,11 +1,20 @@
 "use strict";
 
-const btn = document.getElementsByTagName("button")[0];
-const txtarea = document.getElementsByTagName("textarea")[0];
+// const btn = document.getElementsByTagName("button")[0];
+// const txtarea = document.getElementsByTagName("textarea")[0];
+const btn = document.querySelector("button");
+const txtarea = document.querySelector("textarea");
 
 btn.addEventListener("click", () => {
-  for (const w of txtarea.value.trim().split("\n")) {
+  for (const [i, w] of txtarea.value
+    .toLowerCase()
+    .trim()
+    .split("\n")
+    .entries()) {
     const [w1, w2] = w.trim().split("_");
-    console.log(w1.toLowerCase() + w2.replace(w2[0], w2[0].toUpperCase()));
+    console.log(
+      `${w1}${w2.replace(w2[0], w2[0].toUpperCase())}`.padEnd(20) +
+        `${"✅".repeat(i + 1)}`
+    );
   }
 });
