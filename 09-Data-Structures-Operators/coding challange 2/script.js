f55b1876afd63f58244fb0a65bb738c8639d244f
Michal Grega <michal@gregasr.eu>
2022-08-13 19:37:25 +0200

    finished

--- 09-Data-Structures-Operators/coding challange 2/script.js
+++ 09-Data-Structures-Operators/coding challange 2/script.js
@@ -1,7 +1,11 @@
 "use strict";
 
 const btn = document.getElementsByTagName("button")[0];
+const txtarea = document.getElementsByTagName("textarea")[0];
 
 btn.addEventListener("click", () => {
-  console.log("clicked");
+  for (const w of txtarea.value.trim().split("\n")) {
+    const [w1, w2] = w.trim().split("_");
+    console.log(w1.toLowerCase() + w2.replace(w2[0], w2[0].toUpperCase()));
+  }
 });
