c47f4f1f8e2888c0d5470df60dde3b983823a77d
Michal Grega <michal@gregasr.eu>
2022-08-14 19:13:33 +0200

    Callback functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -29,6 +29,7 @@ createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
 */
 
+/* Value vs reference
 const flight = 'LH234';
 const jonas = {
   name: 'Jonas Schmedtmann',
@@ -56,3 +57,34 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+*/
+
+/*
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+const high5 = function () {
+  console.log('✋🏻');
+};
+
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Marha', 'Adam'].forEach(high5);
+*/
